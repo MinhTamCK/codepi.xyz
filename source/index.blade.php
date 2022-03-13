69e6b372c76c8ce08c269d8b8a9fa3d818378cf1
MinhTamCK <minhtamck4@gmail.com>
2022-03-13 10:57:30 +0700

    update: getting and some configs

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,21 +1,9 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static</h1>
-
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
-
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
-
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
-    </a>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+<h1>Welcome to codepi.xyz</h1>
+<p>I'm Tam and I'm frontend developer.</p>
+<p>I like creation and new technical, I'm want to write some notes, experience when I'm working and share to everyone.
+<p>Currently, I'm working at Ho Chi Minh city.</p>
+</p>
 @endsection
