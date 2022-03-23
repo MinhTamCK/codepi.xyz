04e10eee799af14201554785b37b1c57dadd6322
MinhTamCK <minhtamck4@gmail.com>
2022-03-23 14:18:53 +0700

    edit: about page

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,14 +3,16 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
+<h1>About</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+<p>My name is {{ $page->owner->name }}. I'm a web developer with over 7 years of experience. Enthusiastic frontend developer who loves to build high impact, meaningful products, enjoys working with flexible times, places.</p>
 
-    <h2>Links:</h2>
+<h2>Links:</h2>
 
-    <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
-    </ul>
+<ul>
+    <li><a href="mailto:minhtamck4@gmail.com">Gmail: minhtamck4@gmail.com</a></li>
+    <li><a href="http://minhtamck.github.io" target="_blank">Personal website</a></li>
+    <li><a href="/twitter" target="_blank">Twitter</a></li>
+    <li><a href="/github" target="_blank">GitHub</a></li>
+</ul>
 @endsection
