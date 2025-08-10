930a8225f8840627f62a6ced875c259d04bedb19
tam <minhtamck4@gmail.com>
2025-08-10 20:24:52 +0700

    Update index.blade.php
    Update greeting.

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,8 +1,8 @@
 @extends('_layouts.master')
 
 @section('content')
-<h1>Welcome to codepi.xyz</h1>
-<p>I'm Tam and I'm frontend developer.</p>
-<p>I like creating new technologies, I'm want to write some notes, experience when I'm working, and share with everyone.</p>
-<p>Currently, I'm working at Ho Chi Minh city.</p>
+<h1>Hi, I’m Tam — frontend developer</h1>
+<p>Welcome to <strong>codepi.xyz</strong></p>
+<p>I enjoy creating new technologies and want to share my notes and experiences from my work with everyone.</p>
+<p>Currently, I’m based in Ho Chi Minh City.</p>
 @endsection
