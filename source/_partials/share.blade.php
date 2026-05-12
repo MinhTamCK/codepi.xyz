e32d9602d6d049b456a0ac5f7b14de96426c9052
Tam Nguyen <minhtamck4@gmail.com>
2026-05-12 08:23:42 +0000

    Redesign: minimalist editorial style
    - Replace Tacit CSS with custom SCSS (Newsreader serif + Inter sans)
    - 680px content column, generous whitespace, 1.7 line-height
    - Light/dark mode (auto + manual toggle, persisted in localStorage)
    - Rust accent (#b8472a light / #d97757 dark)
    - Header: brand + Posts/About/RSS + theme toggle
    - Homepage: bio intro + 8 recent posts
    - Posts page: grouped by year, date-prefix list
    - Post page: cleaner metadata, contextual "X years old" notice
    - Outlined share buttons, refined form styles

--- source/_partials/share.blade.php
+++ source/_partials/share.blade.php
@@ -1,15 +1,6 @@
-<button class="share-button" data-sharer="facebook" data-url="{{ $page->getUrl() }}" style="background: #3b5998;">
-    Facebook
-</button>
-
-<button class="share-button" data-sharer="twitter" data-url="{{ $page->getUrl() }}" data-title="{{ $page->title }}" style="background: #00aced;">
-    Twitter
-</button>
-
-<button class="share-button" data-sharer="reddit" data-url="{{ $page->getUrl() }}" style="background: #ff4500;">
-    Reddit
-</button>
-
-<button class="share-button" data-sharer="linkedin" data-url="{{ $page->getUrl() }}" style="background: #0077b5;">
-    LinkedIn
-</button>
+<div class="share-buttons">
+    <button class="share-button" data-sharer="twitter" data-url="{{ $page->getUrl() }}" data-title="{{ $page->title }}">Twitter</button>
+    <button class="share-button" data-sharer="linkedin" data-url="{{ $page->getUrl() }}">LinkedIn</button>
+    <button class="share-button" data-sharer="facebook" data-url="{{ $page->getUrl() }}">Facebook</button>
+    <button class="share-button" data-sharer="reddit" data-url="{{ $page->getUrl() }}">Reddit</button>
+</div>
--- source/posts.blade.php
+++ source/posts.blade.php
@@ -3,16 +3,24 @@
 @section('title', 'Posts')
 
 @section('content')
-    <h1>Posts</h1>
+    <h1>Writing</h1>
+    <p class="posts-summary">{{ $posts->count() }} posts since {{ date('Y', $posts->min('date')) }}.</p>
 
-    <ul>
-        @forelse ($posts->sortByDesc('date') as $post)
-            <li>
-                <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
-                <small>{{ $post->prettyDate() }}</small>
-            </li>
-        @empty
-            <p>No posts to show.</p>
-        @endforelse
-    </ul>
+    @php
+        $byYear = $posts->sortByDesc('date')->groupBy(function ($p) {
+            return date('Y', $p->date);
+        });
+    @endphp
+
+    @foreach ($byYear as $year => $yearPosts)
+        <h2 class="year-heading">{{ $year }}</h2>
+        <ul class="post-list">
+            @foreach ($yearPosts as $post)
+                <li>
+                    <span class="date">{{ date('M j', $post->date) }}</span>
+                    <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endforeach
 @endsection
